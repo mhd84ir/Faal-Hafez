49f2b7be50e685b7b75438e924b8fc9ed8f00c4c
mhd84ir <mhd84.ir@gmail.com>
2024-06-08 14:06:40 +0330

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        FaalGUI faalGUI = new FaalGUI();
+    }
+}
